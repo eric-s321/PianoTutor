ed80b6af2c3a09c77ede6837f8bdb608557fea0d
Justin Diaz <justin529@hotmail.com>
2016-11-05 17:10:44 -0400

    Got rid of infinite loop for keyboard display and added start button functionality

--- pianohack.py
+++ pianohack.py
@@ -10,91 +10,98 @@ clicked = []
 clock = pygame.time.Clock()
 class keyboard(pygame.sprite.Sprite):
     def __init__(self):
+            print("redrawing keyboard")
             super().__init__()
-            pygame.draw.rect(gdisplay, white, (180,100,50,100))
-            pygame.draw.rect(gdisplay, black, (180,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (230,100,50,100))
-            pygame.draw.rect(gdisplay, black, (230,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (280,100,50,100))
-            pygame.draw.rect(gdisplay, black, (280,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (330,100,50,100))
-            pygame.draw.rect(gdisplay, black, (330,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (380,100,50,100))
-            pygame.draw.rect(gdisplay, black, (380,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (430,100,50,100))
-            pygame.draw.rect(gdisplay, black, (430,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (480,100,50,100))
-            pygame.draw.rect(gdisplay, black, (480,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (530,100,50,100))
-            pygame.draw.rect(gdisplay, black, (530,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (580,100,50,100))
-            pygame.draw.rect(gdisplay, black, (580,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (630,100,50,100))
-            pygame.draw.rect(gdisplay, black, (630,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (680,100,50,100))
-            pygame.draw.rect(gdisplay, black, (680,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (730,100,50,100))
-            pygame.draw.rect(gdisplay, black, (730,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (780,100,50,100))
-            pygame.draw.rect(gdisplay, black, (780,100,50,100), 1)
-            pygame.draw.rect(gdisplay, white, (830,100,50,100))
-            pygame.draw.rect(gdisplay, black, (830,100,50,100), 1)
+            x = 180
+            
+            for i in range(14):
+        
+                pygame.draw.rect(gdisplay, white, (x,100,50,100))
+                pygame.draw.rect(gdisplay, black, (x,100,50,100), 1)
+                x += 50
+     
             pygame.draw.rect(gdisplay, black, (220,100,30,55))
             pygame.draw.rect(gdisplay, black, (270,100,30,55))
+            
             pygame.draw.rect(gdisplay, black, (370, 100, 30,55))
             pygame.draw.rect(gdisplay, black, (420,100,30,55))
             pygame.draw.rect(gdisplay, black, (470, 100, 30,55))
+            
             pygame.draw.rect(gdisplay, black, (570,100,30,55))
             pygame.draw.rect(gdisplay, black, (670,100,30,55))
+            
             pygame.draw.rect(gdisplay, black, (770, 100, 30,55))
             pygame.draw.rect(gdisplay, black, (820,100,30,55))
             pygame.draw.rect(gdisplay, black, (620, 100, 30,55))
 
+#def keyred():
+    #if input from keyboard == (coordinates):
+        #pygame.draw.rect(gdisplay, red, (coordinates))
+            
 class button(pygame.sprite.Sprite):
      def __init__(self):
         super().__init__()
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-
-
+        
+        # if mouse is in button rectangle area the rectangle turns light red
         if 500+100 > mouse[0] > 500 and 550+50 > mouse[1] > 550:
             pygame.draw.rect(gdisplay, red, (500,550, 100, 50))
-
+           
         else:
             pygame.draw.rect(gdisplay, lightred, (500,550, 100, 50))
 
         font2 = pygame.font.Font("freesansbold.ttf",20)
         btext= font2.render("Start", 1,(0,0,0))
         gdisplay.blit(btext, (505, 560))
-
-
-
+        
+def main_screen():
+    crash = True
+    gdisplay.fill(white)
+    keyboard()
+    pygame.display.update()
+    while crash:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                crash = False
+      
+        
 def start_screen():
     intro = True
+    # if user clicks x in window the game exits
     while intro:
+        gdisplay.fill(white)
+        font=pygame.font.Font(None,90)
+        scoretext=font.render("Piano Tutor", 10,(0,0,0))
+        gdisplay.blit(scoretext, (500, 300))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
                 intro = False
-
-        gdisplay.fill(white)
-        font=pygame.font.Font(None,90)
-        scoretext=font.render("Placeholder", 10,(0,0,0))
-        gdisplay.blit(scoretext, (100, 300))
+    
+        
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-
+        
         button()
+        #if mouse is clicked within area of rectangle the screen changes
         if 500+100 > mouse[0] > 500 and 550+50 > mouse[1] > 550:
             if click == (1,0,0):
                     clicked.append(button)
         if button in clicked:
             gdisplay.fill(white)
-            return
-        keyboard()
+            main_screen()
+            
+        
         pygame.display.update()
         clock.tick(60)
 start_screen()
+
+
+
+
 pygame.quit
 quit()
